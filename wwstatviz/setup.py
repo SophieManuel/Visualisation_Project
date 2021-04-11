bf3c837f9c7aacedce1a1eb760d275bf9081f4fb
Sophie Manuel <sophie.manuel@etu.umontpellier.fr>
2021-04-11 12:28:44 +0200

    A way to load files

--- wwstatviz/setup.py
+++ wwstatviz/setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup, find_packages
 
+
 def get_long_description():
     with open('README.md', 'r') as f:
         long_description = f.read()
     return long_description
 
+
 setup(
-    name = 'wwstatviz', 
-    version = '0.1', 
-    license = 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    description = 'A package for world wide statistics visualizer', 
-    package_dir = {'': 'src'}, 
-    packages = find_packages(where = 'src'), 
-    classifiers = [
-        # 'Development Status :: 5 - Production/Stable', 
-        'Intended Audience :: Developers', 
-        'Intended Audience :: Science/Research', 
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 
-        'Programming Language :: Python :: 3', 
-        'Operating System :: OS Independent', 
-    ], 
-    long_description = get_long_description(), 
-    long_description_content_type = 'text/markdown', 
-    url = 'https://github.com/SophieManuel/Worldwide_statistics', 
-    author = 'Anas Zakroum, Sophie Manuel, Ravahere Paint-Koui, Seydou Sane', 
-    author_email = 'anaszkrm@protonmail.com', 
-    keywords = 'country map visualization statistics data', 
+    name='wwstatviz',
+    version='0.1',
+    license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    description='A package for world wide statistics visualizer',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SophieManuel/Worldwide_statistics',
+    author='Anas Zakroum, Sophie Manuel, Ravahere Paint-Koui, Seydou Sane',
+    author_email='anaszkrm@protonmail.com',
+    keywords='country map visualization statistics data'
 )

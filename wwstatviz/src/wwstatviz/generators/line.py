b9b1f22800a550037a18a17571a6d81f8be91793
Ravahere Paint-Koui <ravahere.paint-koui@etu.umontpellier.fr>
2021-04-20 00:03:07 +0200

    added legend to line plot and fixed bug (idx => cols)

--- wwstatviz/src/wwstatviz/generators/line.py
+++ wwstatviz/src/wwstatviz/generators/line.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class LineGenerator(Generator):
 
-    def __init__(self, data, countries = 'all', features = 'all'):
+    def __init__(self, data, countries = 'all', features = 'all', legend = False):
         super().__init__(data)
         ### checking arguments
         # countries
@@ -25,8 +25,12 @@ class LineGenerator(Generator):
             for e in features:
                 if e not in self._data.columns.tolist():
                     raise ValueError(f'encountred invalid feature name: {e}')
+        # legend
+        if not isinstance(legend, bool):
+            raise ValueError('invalid argument legend')
         self._countries = countries
         self._features = features
+        self._legend = legend
 
     def generate(self):
         ### get data
@@ -41,10 +45,12 @@ class LineGenerator(Generator):
         df = self._data.loc[idx, cols]
         ### generate line plots
         fig = plt.figure()
-        x = np.arange(len(idx))
+        x = np.arange(len(cols))
         for i in idx:
             y = df.loc[i].values
             plt.plot(x, y, '-')
+        if self._legend:
+            fig.legend(idx)
         ### setting figure
         self._figure = fig
         return self._figure

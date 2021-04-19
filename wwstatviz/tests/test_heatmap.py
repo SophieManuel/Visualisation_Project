607a5473e83aa5072b2479e6bf726d55e1c960ce
SeydouSANE <seydou.sane@etu.umontpellier.fr>
2021-04-20 00:53:06 +0200

    added unit test using pytest

---
+++ wwstatviz/tests/test_heatmap.py
@@ -0,0 +1,12 @@
+from wwstatviz import Visualizer
+import matplotlib
+from pathlib import Path
+
+def test_heatmap():
+    v = Visualizer('/workspace/data/test_cc_3d.csv')
+    fig = v.heatmap(title = 'This is a test heatmap',
+                    xlabel = 'Countries', ylabel = 'Countries')
+    assert fig.figure is not None
+    assert isinstance(fig.figure, matplotlib.figure.Figure)
+    fig.save('test_heatmap.png')
+    assert Path('test_heatmap.png').is_file()

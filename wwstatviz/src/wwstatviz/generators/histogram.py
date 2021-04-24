c97c528b6fd2b03a87dce0678a88c29a20cadff4
Sophie Manuel <sophie.manuel@etu.umontpellier.fr>
2021-04-24 18:46:49 +0200

    added arg checking from the generators

--- wwstatviz/src/wwstatviz/generators/histogram.py
+++ wwstatviz/src/wwstatviz/generators/histogram.py
@@ -8,6 +8,28 @@ class HistogramGenerator(Generator):
 
     def __init__(self, data, countries='all', features='all', legend=False):
         super().__init__(data)
+        ### checking arguments
+        # countries
+        if not isinstance(countries, str) and not isinstance(countries, list):
+            raise ValueError('invalid countries argument')
+        if isinstance(countries, str) and countries != 'all':
+            raise ValueError('invalid countries argument')
+        if isinstance(countries, list):
+            for e in countries:
+                if e not in self._data.index.tolist():
+                    raise ValueError(f'encountred invalid country code: {e}')
+        # features
+        if not isinstance(features, str) and not isinstance(features, list):
+            raise ValueError('invalid features argument')
+        if isinstance(features, str) and features != 'all':
+            raise ValueError('invalid features argument')
+        if isinstance(features, list):
+            for e in features:
+                if e not in self._data.columns.tolist():
+                    raise ValueError(f'encountred invalid feature name: {e}')
+        # legend
+        if not isinstance(legend, bool):
+            raise ValueError('invalid argument legend')
         self._countries = countries
         self._features = features
         self._legend = legend
